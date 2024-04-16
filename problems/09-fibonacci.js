0bd545f5fcd1ab64689f2af013f5cd949c98aa6b
Laiba Afzal <laiba.a1298@gmail.com>
2024-04-16 16:22:10 -0400

    09

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -21,7 +21,16 @@ fibonacci(10); // 55
 
 function fibonacci(n) {
   // Your code here 
+  if (n === 1 || n === 2) return 1;
+  return fibonacci(n-1) + fibonacci(n-2);
 }
+
+// fibonacci(1); // 1
+// fibonacci(2); // 1
+console.log(fibonacci(3)); // 2
+// fibonacci(4); // 3
+// fibonacci(10); // 55
+
   
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = fibonacci;

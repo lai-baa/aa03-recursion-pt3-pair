2bf0d7b7f4c9caaa59b538519a4310838e6d3e01
rcerney <ryanccerney@gmail.com>
2024-04-15 23:16:14 -0700

    problem 12 done

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -4,18 +4,24 @@ is sorted in ascending order.
 
 Examples:
 
-isSorted([1, 2, 3, 4, 5]); // true
-isSorted([1, 2, 4, 3, 5]); // false
-isSorted([2, 4, 6, 7, 8]); // true
-isSorted([5, 4, 3, 2, 1]); // false
+e
 ***********************************************************************/
 
 
 function isSorted(arr) {
-  // Your code here 
+  // Your code here
+  if(arr.length <= 1) return true; 
+  if(arr[0] < arr[1]) {
+    return isSorted(arr.slice(1));
+  } else {
+    return false;
+  }
 }
 
-
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+// isSorted([2, 4, 6, 7, 8]); // true
+// isSorted([5, 4, 3, 2, 1]); // fals
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = isSorted;
